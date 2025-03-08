2ea0d984429d1c2e809f03cb0b808a9775fef352
Em Pongpat <empongpat@gmail.com>
2025-03-08 23:30:08 +0800

    feat: create Xcode Swift app project

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Pongpat Weesommai on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
