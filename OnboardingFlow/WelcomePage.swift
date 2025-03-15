cb70f1239033524c0707d59a40b73a7cdb4d5a0a
Em Pongpat <empongpat@gmail.com>
2025-03-15 22:26:41 +0800

    feat: adds borders and app icon image

--- OnboardingFlow/WelcomePage.swift
+++ OnboardingFlow/WelcomePage.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 struct WelcomePage: View {
     var body: some View {
-        Text("Welcome to MyApp")
-            .font(Font.title)
-            .fontDesign(Font.Design.rounded)
-            .fontWeight(.semibold)
+        VStack {
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 150, height: 150)
+                .foregroundStyle(.tint)
+            
+            Text("Welcome to MyApp")
+                .font(Font.title)
+                .fontDesign(Font.Design.rounded)
+                .fontWeight(.semibold)
+                .border(.black, width: 1.5)
+            
+            Text("Description Text")
+                .font(.title2)
+                .border(.black, width: 1.5)
+        }
+        .border(.orange, width: 1.5)
+        .padding()
+        .border(.purple, width: 1.5)
     }
 }
 
