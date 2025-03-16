5d9c052e36c5043a4efa155873c81bf58ac0e9a5
Em Pongpat <empongpat@gmail.com>
2025-03-16 22:35:47 +0800

    feat: adds ZStack to to embellish the graphic icon

--- OnboardingFlow/WelcomePage.swift
+++ OnboardingFlow/WelcomePage.swift
@@ -10,23 +10,26 @@ import SwiftUI
 struct WelcomePage: View {
     var body: some View {
         VStack {
-            RoundedRectangle(cornerRadius: 30)
-                .frame(width: 150, height: 150)
-                .foregroundStyle(.tint)
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 150, height: 150)
+                    .foregroundStyle(.tint)
+                Image(systemName: "figure.2.and.child.holdinghands")
+                    .font(.system(size: 70))
+                    .foregroundStyle(.white)
+            }
+            
             
             Text("Welcome to MyApp")
                 .font(Font.title)
                 .fontDesign(Font.Design.rounded)
                 .fontWeight(.semibold)
-                .border(.black, width: 1.5)
+                .padding(.top)
             
             Text("Description Text")
                 .font(.title2)
-                .border(.black, width: 1.5)
         }
-        .border(.orange, width: 1.5)
         .padding()
-        .border(.purple, width: 1.5)
     }
 }
 
